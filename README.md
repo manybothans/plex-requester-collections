202b015528daf3cf04f95a48d168316981518c72
Jess Latimer <jplatimer@gmail.com>
2023-05-12 11:37:24 -0700

    cleaned up overseerr request code

--- README.md
+++ README.md
@@ -1,38 +1,38 @@
-# Plex Requester Collection
+# Plex Requester Collections
 
 This app automatically creates Collections in Plex Media Server for the content that each user requests in the media request management system Overseerr.
 
 ## Installation
 
-Install using NPM:
+After downloading the source code, install dependencies using NPM:
 
 ```bash
 npm i
 ```
 
-<!-- 
-
 ## Usage
 
-```python
-import foobar
+Add a `.env` file to your root project directory that looks like this:
 
-# returns 'words'
-foobar.pluralize('word')
-
-# returns 'geese'
-foobar.pluralize('goose')
-
-# returns 'phenomenon'
-foobar.singularize('phenomena')
+```bash
+NODE_ENV=development
+OVERSEERR_API_KEY=********************
+OVERSEERR_URL=http://overseerr-example.com:5055/api/v1
 ```
 
+Build and run with:
+
+```bash
+npm start
+```
+
+<!--
 ## Contributing
 
 Pull requests are welcome. For major changes, please open an issue first
 to discuss what you would like to change.
 
-Please make sure to update tests as appropriate. 
+Please make sure to update tests as appropriate.
 
 -->
 
--- src/overseerr.ts
+++ src/overseerr.ts
@@ -2,39 +2,41 @@ import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
 
-const OVERSEERR_API_KEY = process.env.OVERSEERR_API_KEY;
-const OVERSEERR_URL = process.env.OVERSEERR_URL;
-
 const OverseerrAPI = {
 	getStatus: async function () {
+		const data = await this.callApi({ url: "/status" });
+		console.log(data);
+		return data;
+	},
+	getRequests: async function (
+		params = {
+			take: 20,
+			skip: 0,
+			sort: "added"
+		}
+	) {
+		const data = await this.callApi({
+			url: "/request",
+			params: params
+		});
+		console.log(data);
+		return data;
+	},
+	callApi: async function (requestObj) {
 		try {
-			const response = await axios.get(OVERSEERR_URL + "/status");
-			console.log(response.data);
+			requestObj.baseURL = process.env.OVERSEERR_URL;
+			requestObj.headers = {
+				"X-Api-Key": process.env.OVERSEERR_API_KEY
+			};
+			if (!requestObj.method) requestObj.method = "get";
+			const response = await axios.request(requestObj);
+			// console.log(response);
+			return response.data;
 		} catch (error) {
 			console.error(error);
+			return null;
 		}
-	},
-	getRequests: async function () {
-		try {
-			const response = await axios.request({
-				method: "get",
-				url: "/request",
-				baseURL: OVERSEERR_URL,
-				headers: {
-					"X-Api-Key": OVERSEERR_API_KEY,
-				},
-				params: {
-					take: 20,
-					skip: 0,
-					sort: "added",
-					requestedBy: 1,
-				},
-			});
-			console.log(response.data);
-		} catch (error) {
-			console.error(error);
-		}
-	},
+	}
 };
 
 export default OverseerrAPI;
