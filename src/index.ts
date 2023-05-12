dc30c19134b9164a9dc0f4bd23a9e73d3ba1f378
Jess Latimer <jplatimer@gmail.com>
2023-05-12 11:48:26 -0700

    removing dist from git

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import OverseerrAPI from "./overseerr";
 
 OverseerrAPI.getRequests();
+OverseerrAPI.getStatus();
--- src/overseerr.ts
+++ src/overseerr.ts
@@ -3,15 +3,19 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const OverseerrAPI = {
+	// Returns the current Overseerr status in a JSON object.
 	getStatus: async function () {
 		const data = await this.callApi({ url: "/status" });
 		console.log(data);
 		return data;
 	},
+	// Returns all requests if the user has the ADMIN or MANAGE_REQUESTS permissions. Otherwise, only the logged-in user's requests are returned.
+	// If the requestedBy parameter is specified, only requests from that particular user ID will be returned.
 	getRequests: async function (
 		params = {
 			take: 20,
 			skip: 0,
+			filter: "available",
 			sort: "added"
 		}
 	) {
@@ -22,6 +26,7 @@ const OverseerrAPI = {
 		console.log(data);
 		return data;
 	},
+	// Abstracted API calls to Overseerr, adds URL and API Key automatically.
 	callApi: async function (requestObj) {
 		try {
 			requestObj.baseURL = process.env.OVERSEERR_URL;
